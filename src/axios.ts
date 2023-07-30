79aeaa9fb2b5f503cb96a30ebdbbc9d191569f0a
bmark210 <baltenco@yandex.ru>
2023-07-30 14:27:49 +0300

    [change] api link

--- src/axios.ts
+++ src/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { InternalAxiosRequestConfig } from "axios";
 
 const instance = axios.create({
-  baseURL: "instagram-server-production.up.railway.app/",
+  baseURL: "https://instagram-server-production.up.railway.app/",
 });
 
 instance.interceptors.request.use((config): InternalAxiosRequestConfig => {
