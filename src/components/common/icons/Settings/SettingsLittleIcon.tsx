f4fc5c04d897323f4b650725e12f2053c9ed9645
bmark210 <baltenco@yandex.ru>
2023-09-04 18:15:41 +0300

    [rename] path to fix error

--- src/components/common/icons/Settings/SettingsLittleIcon.tsx
+++ src/components/common/icons/Settings/SettingsLittleIcon.tsx
@@ -2,8 +2,6 @@ const SettingsLittleIcon = () => {
   return (
     <svg
       aria-label="Settings"
-      // color="rgb(0, 0, 0)"
-      // fill="rgb(0, 0, 0)"
       className="text-black-dark dark:text-white"
       height="18"
       role="img"
